de231bde8cffa423b8659c0be902df61a7d65f55
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-29 17:05:54 -0400

    Update compilerbaby.py

--- compilerbaby.py
+++ compilerbaby.py
@@ -1,7 +1,7 @@
 
 data = 0
 START = ["section .text\n", "   global _start  (ld)\n", "\n", "_start:\n", "   mov edx,len\n", "   mov ecs,msg\n", "   mov ebx,1\n", "   mov eax,4\n", "   int	0x80", "/n", "   mov eax,1/n", "   int 0x80/n", "/n", "section	.data"]
-PRINT = ["msg db '", data,"', 0xa  ;string to be printed/n", "len equ $ - msg     ;length of the string"]
+PRINTA = ["msg db '", data,"', 0xa  ;string to be printed\n", "len equ $ - msg     ;length of the string\n"]
 
  
 # Using readlines()
@@ -15,6 +15,7 @@ for line in Lines:
     count += 1
     data = .format(count, line.strip())
     if linearprint == 1:
+      PRINT = ["msg db '", data,"', 0xa  ;string to be printed/n", "len equ $ - msg     ;length of the string\n"]
       file1 = open('compiled.asm', 'a')
       file1.writelines(PRINT)
       file1.close()
@@ -24,3 +25,30 @@ for line in Lines:
       file1.close()
     if data == "Print: ":
       linearprint = 1
+    if data == "section .script":
+       for letter in Lines:
+          printa = 0
+          printb = 0
+          printc = 0
+          if printc == 1:
+             if letter != ";":
+                putdata = putdata + letter
+             else:
+                printc = 0
+                PRINTA = ["msg db '", putdata,"', 0xa  ;string to be printed\n", "len equ $ - msg     ;length of the string\n"]
+                file1 = open('compiled.asm', 'a')
+                file1.writelines(PRINTA)
+                file1.close()
+          if printc != 1:
+             if letter == "p":
+                printa = 1
+             if printa == 1:
+                if letter == "u":
+                   printb = 1
+             if printb == 1:
+                if letter == "t":
+                   printc = 1
+             
+          
+                
+                      
